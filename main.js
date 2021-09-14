1da2a4832420ca4f592a4320475374b6b2b562d1
RedGuy <I.maisuradze31.19@gmail.com>
2021-09-14 17:47:34 +0400

    api.sect

--- main.js
+++ main.js
@@ -161,6 +161,28 @@ const stickyNav = function (entries) {
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
-  rootMargin: `${navHeight}`,
+  rootMargin: `${navHeight}px`,
 });
 headerObserver.observe(header);
+
+// Sections rev
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserer = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserer.observe(section);
+  section.classList.add("section--hidden");
+});
