55f1952300fa2994dd2a9f6a2d37f801bebf8d71
RedGuy <I.maisuradze31.19@gmail.com>
2021-09-14 18:11:23 +0400

    img obs

--- main.js
+++ main.js
@@ -186,3 +186,27 @@ allSections.forEach(function (section) {
   sectionObserer.observe(section);
   section.classList.add("section--hidden");
 });
+
+// img lazy loading
+const imgTargets = document.querySelectorAll("img[data-src]");
+
+const loadImg = function (entries, observe) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
+
+  entry.target.src = entry.target.dataset.src;
+
+  entry.target.addEventListener("load", function () {
+    entry.target.classList.remove("lazy-img");
+  });
+
+  observe.unobserve(entry.target);
+};
+
+const imgObserver = new IntersectionObserver(loadImg, {
+  root: null,
+  threshold: 0,
+  rootMargin: "-200px",
+});
+
+imgTargets.forEach((img) => imgObserver.observe(img));
