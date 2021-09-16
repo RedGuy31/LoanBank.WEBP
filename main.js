51b1bf36555d725b4b9f8fde97f0fbee21737d6d
RedGuy <I.maisuradze31.19@gmail.com>
2021-09-16 17:13:18 +0400

    sliders

--- main.js
+++ main.js
@@ -214,4 +214,41 @@ imgTargets.forEach((img) => imgObserver.observe(img));
 //  IMG Slider
 const slides = document.querySelectorAll(".slide");
 
-slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i}%)`));
+// const slider = document.querySelector(".slider");
+// slider.style.transform = "scale(0.5)";
+// slider.style.overflow = "visible";
+
+let curSlide = 0;
+const maxSlide = slides.length;
+
+const btnLeft = document.querySelector(".slider__btn--left");
+const btnRight = document.querySelector(".slider__btn--right");
+
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+
+goToSlide(0);
+
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+
+btnRight.addEventListener("click", nextSlide);
+btnLeft.addEventListener("click", prevSlide);
